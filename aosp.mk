7a97ce1be344363adaf259389ef41fc1a16f59dd
Thai Nguyen <thai_ng@outlook.com>
2023-11-06 23:03:50 -0800

    use github raw file for ota

--- aosp.mk
+++ aosp.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/ponces/config/common.mk)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://github.com/thai-ng/treble_build_aosp/blob/android-14.0-duo/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/thai-ng/treble_build_aosp/android-14.0-duo/ota.json
